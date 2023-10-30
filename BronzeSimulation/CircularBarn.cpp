2779d2c21eb5fea15b80b5da33193afd6830ea44
Tanzeem Hasan <thasan50@stuy.edu>
2023-10-30 16:54:11 -0400

    Fixed merge

--- BronzeSimulation/CircularBarn.cpp
+++ BronzeSimulation/CircularBarn.cpp
@@ -24,29 +24,4 @@ int main () {
         counter = 0;
     }
     cout << min;
-=======
-#include <cstdio>
-#include <vector>
-using namespace std;
-
-int main () {
-    //freopen("cbarn.in", "r", stdin);
-    //freopen("cbarn.out", "w", stdout);
-    int rooms;
-    cin >> rooms;
-    int total = 0; int temp = 0;
-    vector<int> v(2*rooms);
-    for (int i = 0; i < rooms; i++) {
-        cin >> v[i];
-    }
-    v[rooms, 2*rooms - 1] = v[0, rooms];
-    for (int j = 0; j < rooms; j++) {
-        for (int k = j; k < j + rooms; k++) {
-            
-        }
-    }
-
-    //Check which leads to the smallest total, multiply each index of v by an increasing number and add them all up,
-    //compare sums for the smallest. 
->>>>>>> bc544f8ba3d92d7f59c7337d4a09e9b689151567
 }

dae6538c9239d3c33864df8fb3333c19ac83b5e7
Tanzeem Hasan <thasan50@stuy.edu>
2023-11-13 17:01:56 -0500

    Work

--- SilverProblemsICD/NtarsisandOdd.cpp
+++ SilverProblemsICD/NtarsisandOdd.cpp
@@ -25,3 +25,9 @@ int main() {
     }
     cout << index;
 }
+
+/* Okay, so let's see. You're given an array, look for the first occurence of an odd number and use that to determine the largest subarrays 
+on its left side and right side. Take the max of left and right side and that's your answer. How does this overlap with checking for the 
+last occurence of an odd number, is it possible for that to lead to a different max length subarray or...? Hold on, we can reason this out
+at home when my brain works again. Learned a lot about number theory and prefix sums today. 
+*/

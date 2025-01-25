1bcc1924f00a98c4aa18de784e60e251d49c050d
thasan50 <thasan3068@gmail.com>
2025-01-25 13:01:00 -0500

    Add old work

---
+++ CompetitiveProgramming/CSES/IntroductoryCSES/CoinPiles.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (a == b && a % 3 == 0) {
+            cout << "YES" << endl;
+        }
+        else if () {
+            
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+}
